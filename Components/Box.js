d61d9a9bcbe83dd345bb6b056f25f8195656e53a
hamzashadeez <hahmad11@gmail.com>
2021-06-25 02:37:18 -0700

    Done with Home Screen

--- Components/Box.js
+++ Components/Box.js
@@ -1,10 +1,19 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
-const Box = () => {
+const Box = ({ book }) => {
+  useEffect(() => {
+    console.log(book);
+  }, []);
   return (
     <View style={styles.box}>
-      <Text>Box</Text>
+      <ImageBackground
+        resizeMode="cover"
+        style={styles.bg}
+        source={{ uri: book?.photo }}
+      >
+        <Text style={{ fontFamily: "lato", color: "#fff" }}>{book?.title}</Text>
+      </ImageBackground>
     </View>
   );
 };
@@ -15,10 +24,18 @@ const styles = StyleSheet.create({
   box: {
     height: 160,
     width: 105,
-    backgroundColor: "#c4c4c4",
+    backgroundColor: "#c7c4c4",
     marginRight: 15,
     borderRadius: 10,
+    elevation: 4
+  },
+
+  bg: {
+    flex: 1,
+    width: "100%",
+    // borderRadius: 10,
+    paddingBottom: 10,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "flex-end",
   },
 });
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,103 +1,116 @@
-import React from 'react'
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import Box from '../Components/Box'
-import HomeHeader from '../Components/HomeHeader'
+import React, { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Box from "../Components/Box";
+import HomeHeader from "../Components/HomeHeader";
+import { db } from "../Configs/firebase";
 
 const Home = () => {
-    return (
-        <View style={styles.screen}>
-            <HomeHeader />
-            <ScrollView style={styles.main} showsVerticalScrollIndicator={false}>
-                <Text style={styles.desc}>Zabi bangaren da kake so</Text>
-                {/* Category New Arrival */}
-                <View style={styles.category}>
-                    <Text style={{fontFamily: "Lato", color: "#222"}}>NEW</Text>
-                    <ScrollView snapToInterval={100} style={styles.list} showsHorizontalScrollIndicator={false} horizontal>
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                    </ScrollView>
-                </View>
-                {/* Category Soyayyah */}
-                <View style={styles.category}>
-                    <Text style={{fontFamily: "Lato", color: "#222"}}>SOYAYYAH</Text>
-                    <ScrollView snapToInterval={100} style={styles.list} showsHorizontalScrollIndicator={false} horizontal>
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                    </ScrollView>
-                </View>
-                {/* Category Rayuwa */}
-                <View style={styles.category}>
-                    <Text style={{fontFamily: "Lato", color: "#222"}}>RAYUWA</Text>
-                    <ScrollView snapToInterval={100} style={styles.list} showsHorizontalScrollIndicator={false} horizontal>
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                    </ScrollView>
-                </View>
-                {/* Category Soyayyah */}
-                <View style={styles.category}>
-                    <Text style={{fontFamily: "Lato", color: "#222"}}>DOGON ZANGO (SERIES)</Text>
-                    <ScrollView snapToInterval={100} style={styles.list} showsHorizontalScrollIndicator={false} horizontal>
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                        <Box />
-                    </ScrollView>
-                </View>
-            </ScrollView>
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    db.collection("Stories").onSnapshot((shot) => {
+      setBooks(
+        shot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
+  return (
+    <View style={styles.screen}>
+      <HomeHeader />
+      <ScrollView style={styles.main} showsVerticalScrollIndicator={false}>
+        <Text style={styles.desc}>Zabi bangaren da kake so</Text>
+        {/* Category New Arrival */}
+        <View style={styles.category}>
+          <Text style={{ fontFamily: "Lato", color: "#222" }}>NEW</Text>
+          <ScrollView
+            snapToInterval={100}
+            style={styles.list}
+            showsHorizontalScrollIndicator={false}
+            horizontal
+          >
+            {books.map((book) => (
+              <Box key={book.id} book={book.data} />
+            ))}
+          </ScrollView>
         </View>
-    )
-}
+        {/* Category Soyayyah */}
+        <View style={styles.category}>
+          <Text style={{ fontFamily: "Lato", color: "#222" }}>SOYAYYAH</Text>
+          <ScrollView
+            snapToInterval={100}
+            style={styles.list}
+            showsHorizontalScrollIndicator={false}
+            horizontal
+          >
+            {books.map((book) => {
+              if (book.data.category === "soyayyah") {
+                return <Box key={book.id} book={book.data} />;
+              }
+            })}
+          </ScrollView>
+        </View>
+        {/* Category Rayuwa */}
+        <View style={styles.category}>
+          <Text style={{ fontFamily: "Lato", color: "#222" }}>RAYUWA</Text>
+          <ScrollView
+            snapToInterval={100}
+            style={styles.list}
+            showsHorizontalScrollIndicator={false}
+            horizontal
+          >
+            {books.map((book) => {
+              if (book.data.category === "rayuwa") {
+                return <Box key={book.id} book={book.data} />;
+              }
+            })}
+          </ScrollView>
+        </View>
+        {/* Category Soyayyah */}
+        <View style={styles.category}>
+          <Text style={{ fontFamily: "Lato", color: "#222" }}>
+            DOGON ZANGO (SERIES)
+          </Text>
+          <ScrollView
+            snapToInterval={100}
+            style={styles.list}
+            showsHorizontalScrollIndicator={false}
+            horizontal
+          >
+            {books.map((book) => (
+              <Box key={book.id} book={book.data} />
+            ))}
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-    screen:{
-        backgroundColor: "white",
-        flex: 1,
-    },
-    main:{
-        flex: 1,
-        padding: 10
-    },
-    desc:{
-        textTransform: "uppercase",
-        fontSize: 10,
-        color: '#ccc',
-        marginBottom: 8
-    },
-    category:{
-        marginBottom: 20
-    },
-    list:{
-        // height: 150,
-        width: "100%",
-        marginTop: 3
-    },
-    
-})
+  screen: {
+    backgroundColor: "white",
+    flex: 1,
+  },
+  main: {
+    flex: 1,
+    padding: 10,
+  },
+  desc: {
+    textTransform: "uppercase",
+    fontSize: 10,
+    color: "#ccc",
+    marginBottom: 8,
+  },
+  category: {
+    marginBottom: 20,
+  },
+  list: {
+    // height: 150,
+    width: "100%",
+    marginTop: 3,
+  },
+});
